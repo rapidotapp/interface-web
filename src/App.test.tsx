7fb8db0db295c0e64e864da62c7b0b40d8aa977a
Safin Singh <safin.singh@gmail.com>
2020-07-25 16:34:46 -0700

    :construction_worker: Fix title on Jest sample test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('Renders Rapid header', () => {
   const { getByText } = render(<App />)
   const linkElement = getByText(/Rapid/i)
   expect(linkElement).toBeInTheDocument()
