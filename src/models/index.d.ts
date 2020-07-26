1921bfc5516639786f0860801c804c07cef46382
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-25 22:14:46 -0400

    typedecs

---
+++ src/models/index.d.ts
@@ -0,0 +1,30 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+export declare class Blog {
+  readonly id: string;
+  readonly name: string;
+  readonly posts?: Post[];
+  constructor(init: ModelInit<Blog>);
+  static copyOf(source: Blog, mutator: (draft: MutableModel<Blog>) => MutableModel<Blog> | void): Blog;
+}
+
+export declare class Post {
+  readonly id: string;
+  readonly title: string;
+  readonly blog?: Blog;
+  readonly comments?: Comment[];
+  constructor(init: ModelInit<Post>);
+  static copyOf(source: Post, mutator: (draft: MutableModel<Post>) => MutableModel<Post> | void): Post;
+}
+
+export declare class Comment {
+  readonly id: string;
+  readonly post?: Post;
+  readonly content: string;
+  constructor(init: ModelInit<Comment>);
+  static copyOf(source: Comment, mutator: (draft: MutableModel<Comment>) => MutableModel<Comment> | void): Comment;
+}
