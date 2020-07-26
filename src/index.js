969c6d1f22eed16b34123420f265883d05d4d2c9
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-25 21:05:22 -0400

    configure amplify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import Amplify from 'aws-amplify'
+import awsExports from './aws-exports.js'
+
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
