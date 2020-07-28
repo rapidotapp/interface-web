ffc5c943ea51b32d4a30be41a103d7cf7b8f946e
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-07-28 12:32:19 -0700

    Storybook/card (#9)
    * feat: add storybook card component
    
    * fix: border radius values
    
    * fix(config): remove jsconfig file
    
    * fix(dep): typescript version dependency conflict in package.json
    
    * fix(config): tsconfig module key
    
    * fix: build
    
    this modifyies annoying rules in eslint config and fixes errors that
    caused the build to fail
    
    * rename: storybook story file name to better demonstrate purpose
    
    Co-authored-by: Rishi Kothari <itsrishikothari@gmail.com>

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -6,7 +6,7 @@ import {
   DarkMode,
 } from '@chakra-ui/core'
 import React from 'react'
-// eslint-disable-next-line react/prop-types
+
 const ThemedComponent: React.FC = ({ children }: any) => (
   <ThemeProvider theme={theme}>
     <ColorModeProvider value="dark">
