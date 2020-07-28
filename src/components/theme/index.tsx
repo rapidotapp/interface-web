21282377cf2f5be43ad1ce40d2b8bf912f9162aa
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-28 13:47:56 -0400

    update themecomponent

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -3,12 +3,13 @@ import {
   theme,
   ColorModeProvider,
   CSSReset,
+  DarkMode,
 } from '@chakra-ui/core'
 import React from 'react'
 // eslint-disable-next-line react/prop-types
-const ThemedComponent = ({ children }) => (
-  <ThemeProvider theme={theme} dark>
-    <ColorModeProvider>
+const ThemedComponent: React.FC = ({ children }: any) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider value="dark">
       <CSSReset />
       {children}
     </ColorModeProvider>
