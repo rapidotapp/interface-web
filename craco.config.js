9a98e1085dabe1433415378b0a6d6c3756923452
eankeen <24364012+eankeen@users.noreply.github.com>
2020-07-30 11:20:18 -0700

    fix: add alises to jest

--- craco.config.js
+++ craco.config.js
@@ -13,6 +13,20 @@ module.exports = function ({ env }) {
         return webpackConfig
       },
     },
+    jest: {
+      configure: (jestConfig, { env, paths, resolve, rootDir }) => {
+        jestConfig.moduleNameMapper['^~atoms(.*)$'] =
+          '<rootDir>/src/components/atoms$1'
+        jestConfig.moduleNameMapper['^~molecules(.*)$'] =
+          '<rootDir>/src/components/molecules$1'
+        jestConfig.moduleNameMapper['^~organisms(.*)$'] =
+          '<rootDir>/src/components/organisms$1'
+        // this must be added last
+        jestConfig.moduleNameMapper['^~(.*)$'] = '<rootDir>/src$1'
+
+        return jestConfig
+      },
+    },
     devServer: (devServerConfig, { env, paths, proxy, allowedHost }) => {
       // webpackConfig.port doesn't work here
       return devServerConfig
