02675f1a8488ad352e8ab0cbd598628b99a1d029
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-30 10:44:49 -0400

    move to data

---
+++ src/data/Auth/index.ts
@@ -0,0 +1,50 @@
+import { auth } from 'firebase'
+import { computed, observable } from 'mobx'
+
+const defaultSucc = (user: any) => {
+  console.log(`Successfully signed in! \n\n ${JSON.stringify(user, null, 2)}`)
+}
+
+const defaultErr = (err: any) => {
+  console.error(
+    `Could not sign in through google. \n\n ${JSON.stringify(err, null, 2)}`
+  )
+}
+
+class AuthStore {
+  @observable user: any = null
+  @observable authPending = true
+
+  listenToAuth: any
+
+  constructor() {
+    this.listenToAuth = auth().onAuthStateChanged(
+      (user) => {
+        this.user = user
+
+        this.authPending = false
+      },
+      (err) => {
+        console.error(err)
+      }
+    )
+  }
+
+  public async doSignInWithGoogle() {
+    const provider = new auth.GoogleAuthProvider()
+    await auth().signInWithPopup(provider).then(defaultSucc, defaultErr)
+  }
+
+  public async doSignInWithEmail(e: string, p: string) {
+    await auth().signInWithEmailAndPassword(e, p).then(defaultSucc, defaultErr)
+  }
+
+  @computed
+  public get isAuthed() {
+    return !!this.user
+  }
+}
+
+const authStore = new AuthStore()
+
+export default authStore
