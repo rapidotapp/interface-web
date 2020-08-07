ad7f34a44b91caf9dcd5c91ef8e0efd3e89e23c4
Safin Singh <safin.singh@gmail.com>
2020-08-07 12:06:47 -0700

    Fix more build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,7 @@
-import { Box } from '@chakra-ui/core'
 import React, { FunctionComponent } from 'react'
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom'
-
-import ThemeX from './components/theme'
-import Chat from './pages/Chat'
-import Community from './pages/Community'
-import Explore from './pages/Explore'
-import Users from './pages/Users'
 
 const App: FunctionComponent = () => {
-  return (
-    <div className="app">
-      <ThemeX>
-        <Box backgroundColor="#212226">
-          <Router>
-            <div>
-              <Switch>
-                <Route path="/community">
-                  <Community />
-                </Route>
-                <Route path="/users">
-                  <Users />
-                </Route>
-                <Route path="/explore">
-                  <Explore />
-                </Route>
-                <Route path="/chat">
-                  <Chat />
-                </Route>
-                <Route path="/">
-                  <Redirect to="/explore" />
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-        </Box>
-      </ThemeX>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
