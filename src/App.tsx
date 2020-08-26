33bc6456666612e55d7c84a9aadbbb3dafa9ce8f
Rishi Kothari <itsrishikothari@gmail.com>
2020-08-26 17:23:07 -0400

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Flex } from '@chakra-ui/core'
+import { Button, Flex } from '@chakra-ui/core'
 import React, { FunctionComponent } from 'react'
 import { Route, Switch, useLocation } from 'react-router-dom'
 
 import NavSidebar from '~/components/organisms/NavSidebar'
 
+import { AuthContext } from './contexts/Auth'
 import Communities from './pages/Communities'
 import CommunityHome from './pages/CommunityHome'
 import Explore from './pages/Explore'
+
+const AuthPage: React.FC = () => {
+  const auth = React.useContext(AuthContext)
+  return <Button onClick={() => auth.signInWithGoogle()}>Hello</Button>
+}
 
 const App: FunctionComponent = () => {
   const location = useLocation()
@@ -21,14 +27,18 @@ const App: FunctionComponent = () => {
   }
 
   return (
-    <Flex>
-      <NavSidebar active={switchLocation(location.pathname)} />
-      <Switch>
-        <Route exact path="/app/explore" component={Explore} />
-        <Route path="/app/:community/:channel" component={Communities} />
-        <Route exact path="/app/:community" component={CommunityHome} />
-      </Switch>
-    </Flex>
+    <>
+      <Flex>
+        <NavSidebar active={switchLocation(location.pathname)} />
+        <Switch>
+          <Route exact path="/" component={AuthPage} />
+          <Route exact path="/app/explore" component={Explore} />
+
+          <Route path="/app/:community/:channel" component={Communities} />
+          <Route exact path="/app/:community" component={CommunityHome} />
+        </Switch>
+      </Flex>
+    </>
   )
 }
 
